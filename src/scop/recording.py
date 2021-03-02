70cf14d79e74ddd39297b1653f53711498909a9d
Olle Vidner <olle@vidner.se>
2021-03-03 00:43:50 +0100

    Prevent DESIGN_ID from being squeezed away

--- src/scop/recording.py
+++ src/scop/recording.py
@@ -226,4 +226,10 @@ class DatasetRecorder(CaseRecorder):
         pass
 
     def assemble_dataset(self, recording_requester):
-        return xr.concat(self.datasets[recording_requester], dim=DESIGN_ID).squeeze()
+        ds = xr.concat(self.datasets[recording_requester], dim=DESIGN_ID).squeeze()
+
+        # We don't want to squeeze away the DESIGN_ID dim, because of reasons
+        if DESIGN_ID not in ds.dims:
+            ds = ds.expand_dims(DESIGN_ID)
+
+        return ds
