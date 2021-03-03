f2dd1351d9714213a4253a61632d692c2b5d762e
Olle Vidner <olle@vidner.se>
2021-03-03 09:21:05 +0100

    Support Python 3.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
-    python_requires=">=3.8, <4",
+    python_requires=">=3.7, <4",
     setup_requires=["setuptools_scm"],
     # FIXME: this should also list pygmo as a requirement. However, pygmo lacks
     # the correct metadata even when installed with Conda, so it will just break.
