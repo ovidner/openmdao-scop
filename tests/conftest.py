e59487e7f727cfcf3143e776c73a3ce79f817298
Olle Vidner <olle@vidner.se>
2021-08-30 13:49:47 +0200

    Add simple smoke test

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import os
+
+import hypothesis
+import numpy as np
+import pytest
+
+np.seterr(all="warn")
+
+hypothesis.settings.register_profile("fast", max_examples=5)
+hypothesis.settings.register_profile("debugger", report_multiple_bugs=False)
