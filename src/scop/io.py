f1a5f6ec21c3452f5cfed46b783538bb840f1a47
Olle Vidner <olle@vidner.se>
2023-01-06 18:33:02 +0100

    Make both Zarr and NetCDF IO methods available

--- src/scop/io.py
+++ src/scop/io.py
@@ -42,7 +42,7 @@ def jsondecode_attrs(attrs):
     return jsonpickle.decode(attrs["_scop:encoded_attrs"])
 
 
-def dump(ds, path, default_compression="lzf", **kwargs):
+def dump_netcdf(ds: xr.Dataset, path, default_compression="lzf", **kwargs):
     # Make a shallow copy so we don't mangle the attrs of the ds we're dumping.
     enc_ds = ds.copy(deep=False)
 
@@ -55,40 +55,50 @@ def dump(ds, path, default_compression="lzf", **kwargs):
 
     enc_ds.attrs["_scop:encoding_version"] = CURRENT_ENCODING_VERSION
 
+    return enc_ds.to_netcdf(path=path, engine="h5netcdf", invalid_netcdf=True, **kwargs)
+
+
+def dump_zarr(ds, path, **kwargs):
+    # Make a shallow copy so we don't mangle the attrs of the ds we're dumping.
+    enc_ds = ds.copy(deep=False)
+
+    enc_ds.attrs = jsonencode_attrs(enc_ds.attrs)
+
+    for var in enc_ds.variables.values():
+        var.attrs = jsonencode_attrs(var.attrs)
+
+    enc_ds.attrs["_scop:encoding_version"] = CURRENT_ENCODING_VERSION
+
     return enc_ds.to_zarr(path, **kwargs)
+
+
+def _load_postprocess(ds):
+    encoding_version = ds.attrs.pop("_scop:encoding_version", None)
+    if encoding_version is None:
+        raise ValueError("File cannot be recognized as a Scop dataset.")
+    elif encoding_version > CURRENT_ENCODING_VERSION:
+        raise ValueError(
+            f"Dataset has a too new version ({encoding_version}). Maximum supported version is {CURRENT_ENCODING_VERSION}."
+        )
+
+    ds.attrs = jsondecode_attrs(ds.attrs)
+    for var in ds.variables.values():
+        var.attrs = jsondecode_attrs(var.attrs)
+
+    return ds
 
 
 def load_netcdf(path, **kwargs):
     ds = xr.open_dataset(path, engine="h5netcdf", **kwargs)
 
-    encoding_version = ds.attrs.pop("_scop:encoding_version", None)
-    if encoding_version is None:
-        raise ValueError("File cannot be recognized as a Scop dataset.")
-    elif encoding_version > CURRENT_ENCODING_VERSION:
-        raise ValueError(
-            f"Dataset has a too new version ({encoding_version}). Maximum supported version is {CURRENT_ENCODING_VERSION}."
-        )
-
-    ds.attrs = jsondecode_attrs(ds.attrs)
-    for var in ds.variables.values():
-        var.attrs = jsondecode_attrs(var.attrs)
-
-    return ds
+    return _load_postprocess(ds)
 
 
-def load(path, **kwargs):
+def load_zarr(path, **kwargs):
     ds = xr.open_zarr(path, **kwargs)
 
-    encoding_version = ds.attrs.pop("_scop:encoding_version", None)
-    if encoding_version is None:
-        raise ValueError("File cannot be recognized as a Scop dataset.")
-    elif encoding_version > CURRENT_ENCODING_VERSION:
-        raise ValueError(
-            f"Dataset has a too new version ({encoding_version}). Maximum supported version is {CURRENT_ENCODING_VERSION}."
-        )
+    return _load_postprocess(ds)
 
-    ds.attrs = jsondecode_attrs(ds.attrs)
-    for var in ds.variables.values():
-        var.attrs = jsondecode_attrs(var.attrs)
 
-    return ds
+dump = dump_zarr
+load = load_zarr
