b80029a14ba06117fb8a3746910bc886b7ce8103
Olle Vidner <olle@vidner.se>
2023-08-01 17:21:06 +0200

    Add early incarnation of modelling framework

--- src/scop/__init__.py
+++ src/scop/__init__.py
@@ -1,5 +1,17 @@
 from .constants import DESIGN_ID  # noqa
 from .io import dump, dump_netcdf, dump_zarr, load, load_netcdf, load_zarr  # noqa
+from .modelling import (  # noqa
+    EnumSpace,
+    InnumSpace,
+    IntegerSpace,
+    Param,
+    ParamSet,
+    RealSpace,
+    Space,
+    add_input_param,
+    add_output_param,
+    bool_space,
+)
 from .processing import (  # noqa
     constraint_space,
     design_space,
